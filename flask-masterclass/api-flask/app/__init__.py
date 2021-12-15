41c4dcc27c234f9b3386ddbcfe41ac5f43cac253
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-12-15 20:50:27 -0300

    Flask-api, configuração do banco de dados.

---
+++ flask-masterclass/api-flask/app/__init__.py
@@ -0,0 +1,17 @@
+from flask_restful import Api
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app.config import config
+
+
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    app.config.from_object(config[config_name])
+
+    api = Api(app)
+    db.init_app(app)
+
+    return app
